2fd700d9e7931d0471e90da4130ff3527c81d1a5
forevermiku <zhenghr6@mail2.sysu.edu.cn>
2018-06-17 14:01:49 +0800

    Add files via upload

--- PM/src/add_soldier.java
+++ PM/src/add_soldier.java
@@ -1,17 +1,11 @@
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
--- PM/src/delete_soldier.java
+++ PM/src/delete_soldier.java
@@ -1,11 +1,20 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
 
 /**
  * Servlet implementation class delete_soldier
@@ -13,13 +22,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/delete_soldier")
 public class delete_soldier extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private JDBChelper jdbchelper;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public delete_soldier() {
         super();
-        jdbchelper = new JDBChelper();
         // TODO Auto-generated constructor stub
     }
 
@@ -35,7 +42,53 @@ public class delete_soldier extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("soldier_id");
+		final String URL = "jdbc:mysql://222.200.180.59:9000/MBDB?useSSL=false";
+		final String USER = "mbcsdev";
+	    final String PASSWORD = "mbcsdev2018";
+	    try {
+			Class.forName("com.mysql.jdbc.Driver");		//注册驱动
+		} catch (ClassNotFoundException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			response.sendError(500, "JDBC initialization failed");
+		}
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		String acceptjson = "";
+		Connection conn;
+		System.out.println("连接服务器……");
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			Statement stmt = conn.createStatement();
+			acceptjson = request.getParameter("data");		//获得json数据
+			if (!acceptjson.equals("")) {
+				response.setContentType("application/json; charset=utf-8");		//设置相应头的数据类型
+				JSONObject jb = JSONObject.fromObject(acceptjson);		//转换为JSONObject
+				int soldier_id = jb.getInt("soldier_id");
+				ResultSet rs1 = stmt.executeQuery("SELECT * FROM OrgAdminRelationships WHERE leader_sid=" + soldier_id);		//检查民兵是否是负责人
+				if (rs1.next()) {
+					response.sendError(403, "Cannot operate on leaders");
+				}
+				rs1.close();
+				ResultSet rs = stmt.executeQuery("SELECT * FROM Soldiers WHERE soldier_id=" + soldier_id);		//查询民兵是否存在
+				if (rs.next()) {
+					stmt.executeUpdate("DELETE FROM OrgSoldierRelationships WHERE soldier_id=" + soldier_id);
+					stmt.executeUpdate("DELETE FROM SoldierRelationships WHERE lower_sid=" + soldier_id + " or " + "higher_sid=" + soldier_id);
+					stmt.executeUpdate("UPDATE Soldiers SET serve_office_id = 4 WHERE soldier_id = " + soldier_id);
+				} else {
+					response.sendError(400, "User does not exist");
+				}
+				rs.close();
+			} else {
+				response.sendError(400, "no input");
+			}
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			response.sendError(400, "Invalid input value");
+		}
 		
 	}
 
